1571912c963a053ddda7262abfa6b0044ca045ee
cesarhg <rasec722003@yahoo.com.mx>
2014-04-01 13:59:08 -0600

    Ejercicio 3

---
+++ assets/js/curso_3_c.js
@@ -0,0 +1,19 @@
+//Arrays
+//var nombre_array = [valor1, valor2, ...,valorN];
+//alert(nombre_array[0])
+// Crear un array llamado meses y que almacene el nombre de los doce meses del año
+// Mostrar por pantalla los doce nombres utilizando la funcion alert()
+var mes_array = ['Enero', 'Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+
+alert(mes_array[0])
+alert(mes_array[1])
+alert(mes_array[2])
+alert(mes_array[3])
+alert(mes_array[4])
+alert(mes_array[5])
+alert(mes_array[6])
+alert(mes_array[7])
+alert(mes_array[8])
+alert(mes_array[9])
+alert(mes_array[10])
+alert(mes_array[11])
